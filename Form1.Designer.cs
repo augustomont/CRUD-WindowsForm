1e6b9bb26e4de87a418bdb271778060dd2043e93
augustomont <augustoengenharia@outlook.com>
2023-09-15 16:20:33 -0300

    criando mecanismo de busca

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,14 @@
             this.txtTelefone = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.btnSalvar = new System.Windows.Forms.Button();
+            this.lstContatos = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // lbNome
@@ -87,7 +95,7 @@
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(226, 349);
+            this.btnSalvar.Location = new System.Drawing.Point(46, 335);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(114, 35);
             this.btnSalvar.TabIndex = 6;
@@ -95,11 +103,79 @@
             this.btnSalvar.UseVisualStyleBackColor = true;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
+            // lstContatos
+            // 
+            this.lstContatos.AllowColumnReorder = true;
+            this.lstContatos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lstContatos.FullRowSelect = true;
+            this.lstContatos.GridLines = true;
+            this.lstContatos.HideSelection = false;
+            this.lstContatos.LabelEdit = true;
+            this.lstContatos.Location = new System.Drawing.Point(337, 120);
+            this.lstContatos.Name = "lstContatos";
+            this.lstContatos.Size = new System.Drawing.Size(643, 250);
+            this.lstContatos.TabIndex = 7;
+            this.lstContatos.UseCompatibleStateImageBehavior = false;
+            this.lstContatos.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(360, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Buscar Contatos";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(337, 88);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(420, 26);
+            this.txtBuscar.TabIndex = 9;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(763, 85);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(99, 32);
+            this.btnBuscar.TabIndex = 10;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "id";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nome";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "E-mail";
+            this.columnHeader3.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Telefone";
+            this.columnHeader4.Width = 100;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1029, 416);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstContatos);
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.txtTelefone);
@@ -124,6 +200,14 @@
         private System.Windows.Forms.TextBox txtTelefone;
         private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.ListView lstContatos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,18 +15,33 @@ namespace crudWindowsForm
     public partial class Form1 : Form
     {
         //criar uma propriedade
-        MySqlConnection Conexao;
+        private MySqlConnection Conexao; //private para ser visivel apenas dentro da classe
+        //Criar variavel de fonte de dados para inserir no MySqlConnection()
+        private string data_source = "datasource=localhost;username=root;password=1234;database=db_agenda";
+
+
         public Form1()
         {
             InitializeComponent();
+
+            //Todos esses componentes podem ser alterados direto pelas propriedades
+            /*lstContatos.View = View.Details;
+            lstContatos.LabelEdit = true;
+            lstContatos.AllowColumnReorder = true;
+            lstContatos.FullRowSelect = true;
+            lstContatos.GridLines = true;
+
+            lstContatos.Columns.Add("ID");
+            lstContatos.Columns.Add("Nome");
+            lstContatos.Columns.Add("E-Mail");
+            lstContatos.Columns.Add("Telefone");
+            */
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
             try
             {
-                //Criar variavel de fonte de dados para inserir no MySqlConnection()
-                string data_source = "datasource=localhost;username=root;password=1234;database=db_agenda";
                
                 //Criar conexao com mysql
                 Conexao = new MySqlConnection(data_source);
@@ -42,9 +57,56 @@ namespace crudWindowsForm
                 comando.ExecuteReader();
 
                 MessageBox.Show("Inserido com sucesso!");
+
+                
                     
 
             }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            } finally
+            {
+                Conexao.Close();
+                txtNome.Text = "";//limpar os campos depois de adcionado
+                txtEmail.Text = "";
+                txtTelefone.Text = "";
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Conexao = new MySqlConnection(data_source);//criar conexao ao banco
+
+                string q = "'%" + txtBuscar.Text + "%'";//variavel debusca
+                string sql = "SELECT * " + "FROM contatos " + "WHERE nome LIKE " + q + " OR email LIKE " + q;//linha de comando
+
+                Conexao.Open();//abre a conexao
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);//Cria o comando
+
+
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                lstContatos.Items.Clear();//limpar lista antes de exibir busca
+
+                while(reader.Read())//acessa o banco de dados, e salva dentro de row os resultados
+                {
+                    string[] row =
+                    {
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                    };
+
+                    var linha_listview = new ListViewItem(row);//salva a busca em uma variavel dinamica
+
+                    lstContatos.Items.Add(linha_listview);//escreve uma linha com o resultado da busca
+                }
+
+            } catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
             } finally
